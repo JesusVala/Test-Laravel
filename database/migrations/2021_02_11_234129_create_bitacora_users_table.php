bafa727df45572a8060b9a3a0c8de6085aeb6f6c
JesusVal <jesus@agilgob.com>
2021-02-11 17:54:41 -0600

    bitacora table created

---
+++ database/migrations/2021_02_11_234129_create_bitacora_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBitacoraUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bitacora_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user', 256);
+            $table->string('email', 256);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bitacora_users');
+    }
+}
